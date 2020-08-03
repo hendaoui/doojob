6d2c4f48addc88348fc4791081d69928a5cc78f4
hendaoui <Heni.Hendaoui@proxym-it.com>
2020-08-03 13:51:52 +0100

    maps

--- src/screens/Dashboard/map/maps.js
+++ src/screens/Dashboard/map/maps.js
@@ -1,4 +1,4 @@
-import React, {Component, useRef} from 'react';
+import React, {Component, useRef, useEffect} from 'react';
 import styles from '../../../helpers/styles';
 import AppText from '../../../components/AppText';
 import {TouchableOpacity} from 'react-native-gesture-handler';
@@ -14,186 +14,212 @@ import BottomNavigation from '../../../components/BottomNavigation';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import normalize from 'react-native-normalize';
 import {observer, inject} from 'mobx-react';
-import Animated from 'react-native-reanimated';
+import Animated, {block, call} from 'react-native-reanimated';
 import colors from '../../../helpers/colors';
 import moment from 'moment';
 import StarRating from '../../../components/StarRating';
+import 'mobx-react-lite/batchingForReactNative';
 
-@inject('Store')
-@observer
-export default class MapsScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  componentDidMount() {
-    this.props.Store.setActiveTab('maps');
-    this.focusListener = this.props.navigation.addListener('focus', () => {
-      this.onFocusFunction();
+const MapsScreen = ({navigation, Store}) => {
+  let mapIndex = 0;
+  let mapAnimation = new Animated.Value(0);
+
+  useEffect(() => {
+    Store.setActiveTab('maps');
+    const focusListener = navigation.addListener('focus', () => {
+      onFocusFunction();
     });
-    // this.mapAnimation.addListener(({value}) => {
-    //   let index = Math.floor(value / normalize(250) + 0.3);
-    //   if (index >= this.props.Store.issuesList.length) {
-    //     index = this.props.Store.issuesList.length - 1;
-    //   }
-    //   if (index <= 0) {
-    //     index = 0;
-    //   }
+  }, [navigation]);
 
-    //   clearTimeout(regionTimeout);
-
-    //   const regionTimeout = setTimeout(() => {
-    //     if (this.mapIndex === index) {
-    //       this.mapIndex = idnex;
-    //       const {location} = this.props.Store.issuesList[index];
-    //       this._map.current.animateToRegion(
-    //         {
-    //           ...location,
-    //           latitudeDelta: 4,
-    //           longitudeDelta: 4,
-    //         },
-    //         350,
-    //       );
-    //     }
-    //   }, 10);
-    // });
-  }
-
-  onFocusFunction = () => {
-    this.props.Store.setActiveTab('maps');
+  const onFocusFunction = () => {
+    Store.setActiveTab('maps');
   };
 
-  mapMarkers = () => {
-    return this.props.Store.issuesList.map((issue) => (
-      <MapView.Marker
-        key={issue._id}
-        coordinate={{
-          latitude: parseFloat(issue?.location.latitude),
-          longitude: parseFloat(issue?.location.longitude),
-        }}
-        title={issue?.title}>
-        <Animated.View style={styleSheet.markerWrap}>
-          <Animated.Image
-            source={require('../../../assets/img/marker.png')}
-            style={styleSheet.marker}
-            resizeMode="cover"
-          />
-        </Animated.View>
-      </MapView.Marker>
-    ));
+  const mapMarkers = () => {
+    return Store.issuesList.map((issue, index) => {
+      const scaleStyle = {
+        transform: [
+          {
+            scale: interpolations[index].scale,
+          },
+        ],
+      };
+      return (
+        <MapView.Marker
+          key={issue._id}
+          coordinate={{
+            latitude: parseFloat(issue?.location.latitude),
+            longitude: parseFloat(issue?.location.longitude),
+          }}
+          title={issue?.title}>
+          <Animated.View style={styleSheet.markerWrap}>
+            <Animated.Image
+              source={require('../../../assets/img/marker.png')}
+              style={[styleSheet.marker, scaleStyle]}
+              resizeMode="cover"
+            />
+          </Animated.View>
+        </MapView.Marker>
+      );
+    });
   };
 
-  render() {
-    const {navigation} = this.props;
-    return (
-      <>
-        <AppSpinner />
-        <View style={styles.screenWrapperWithRadius}>
-          <View style={{height: normalize(65), paddingRight: normalize(20)}}>
-            <Header nvg={{...navigation}} title="Map" showNotifications />
-          </View>
-          <MapView
-            style={{
-              flex: 1,
-            }}
-            provider={PROVIDER_GOOGLE}
-            showsUserLocation={true}
-            showsMyLocationButton={true}
-            showsCompass={true}
-            toolbarEnabled={true}
-            zoomEnabled={true}
-            rotateEnabled={true}
-            region={{
-              latitude: 36.006389,
-              longitude: 9.521667,
-              latitudeDelta: 4,
-              longitudeDelta: 4,
-            }}
-            onMapReady={() => {
-              PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-              );
-            }}>
-            <this.mapMarkers />
-          </MapView>
-          <Animated.ScrollView
-            horizontal
-            scrollEventThrottle={1}
-            showsHorizontalScrollIndicator={false}
-            style={styleSheet.scrollView}
-            onScroll={Animated.event(
-              [
-                {
-                  nativeEvent: {
-                    contentOffset: {
-                      x: this.mapAnimation,
-                    },
+  const interpolations = Store.issuesList.map((issue, index) => {
+    const inputRange = [
+      (index - 1) * normalize(300),
+      index * normalize(300),
+      (index + 1) * normalize(300),
+    ];
+    const scale = mapAnimation.interpolate({
+      inputRange,
+      outputRange: [1, 1.5, 1],
+      extrapolate: 'clamp',
+    });
+
+    return {scale};
+  });
+
+  const _map = React.useRef(null);
+  const _scrollView = React.useRef(null);
+
+  return (
+    <>
+      <AppSpinner />
+      <View style={styles.screenWrapperWithRadius}>
+        <View style={{height: normalize(65), paddingRight: normalize(20)}}>
+          <Header nvg={{...navigation}} title="Map" showNotifications />
+        </View>
+        <MapView
+          style={{
+            flex: 1,
+          }}
+          ref={_map}
+          provider={PROVIDER_GOOGLE}
+          showsUserLocation={true}
+          showsMyLocationButton={true}
+          showsCompass={true}
+          toolbarEnabled={true}
+          zoomEnabled={true}
+          rotateEnabled={true}
+          region={{
+            latitude: 36.006389,
+            longitude: 9.521667,
+            latitudeDelta: 4,
+            longitudeDelta: 4,
+          }}
+          onMapReady={() => {
+            PermissionsAndroid.request(
+              PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            );
+          }}>
+          {mapMarkers()}
+        </MapView>
+        <Animated.ScrollView
+          ref={_scrollView}
+          horizontal
+          scrollEventThrottle={1}
+          showsHorizontalScrollIndicator={false}
+          style={styleSheet.scrollView}
+          onScroll={Animated.event(
+            [
+              {
+                nativeEvent: {
+                  contentOffset: {
+                    x: (x) =>
+                      Animated.block([
+                        Animated.set(mapAnimation, x),
+                        Animated.call([x], ([value]) => {
+                          let index = Math.floor(value / normalize(300) + 0.3);
+                          if (index >= Store.issuesList.length) {
+                            index = Store.issuesList.length - 1;
+                          }
+                          if (index <= 0) {
+                            index = 0;
+                          }
+                          clearTimeout(regionTimeout);
+                          const regionTimeout = setTimeout(() => {
+                            if (mapIndex !== index) {
+                              mapIndex = index;
+                              const {location} = Store.issuesList[index];
+                              _map.current.animateToRegion(
+                                {
+                                  latitude: parseFloat(location?.latitude),
+                                  longitude: parseFloat(location?.longitude),
+                                  latitudeDelta: 1.5,
+                                  longitudeDelta: 1.5,
+                                },
+                                350,
+                              );
+                            }
+                          }, 10);
+                        }),
+                      ]),
                   },
                 },
-              ],
-              {useNativeDriver: true},
-            )}>
-            {this.props.Store.issuesList.map((issue, index) => (
-              <TouchableOpacity
-                style={styleSheet.card}
-                key={index}
-                activeOpacity={0.8}
-                onPress={() =>
-                  navigation.navigate('issueDetails', {
-                    id: issue?._id,
-                    title: issue?.title,
-                  })
-                }>
+              },
+            ],
+            {useNativeDriver: true},
+          )}>
+          {Store.issuesList.map((issue, index) => (
+            <TouchableOpacity
+              style={styleSheet.card}
+              key={index}
+              activeOpacity={0.8}
+              onPress={() =>
+                navigation.navigate('issueDetails', {
+                  id: issue?._id,
+                  title: issue?.title,
+                })
+              }>
+              <View
+                style={{
+                  flex: 1,
+                  flexDirection: 'row',
+                }}>
+                <View style={{alignSelf: 'center'}}>
+                  <ImageBackground
+                    style={styleSheet.photo}
+                    source={
+                      issue?.photo1
+                        ? {uri: issue?.photo1}
+                        : require('../../../assets/img/no-image.jpg')
+                    }
+                  />
+                </View>
                 <View
                   style={{
                     flex: 1,
-                    flexDirection: 'row',
+                    marginLeft: normalize(10),
+                    alignSelf: 'center',
                   }}>
-                  <View>
-                    <ImageBackground
-                      style={styleSheet.photo}
-                      source={
-                        issue?.photo1
-                          ? {uri: issue?.photo1}
-                          : require('../../../assets/img/no-image.jpg')
-                      }
-                    />
-                  </View>
-                  <View
-                    style={{
-                      flex: 1,
-                      marginLeft: normalize(10),
-                      alignSelf: 'center',
-                    }}>
-                    <AppText style={{fontFamily: 'Poppins-SemiBold'}}>
-                      {issue?.title}
-                    </AppText>
-                    <AppText style={{fontSize: normalize(14)}}>
-                      {issue?.author?.name}
-                    </AppText>
-                    <StarRating ratings={issue?.author?.rating} />
-                  </View>
+                  <AppText style={{fontFamily: 'Poppins-SemiBold'}}>
+                    {issue?.title}
+                  </AppText>
+                  <AppText style={{fontSize: normalize(14)}}>
+                    {issue?.author?.name}
+                  </AppText>
+                  <StarRating ratings={issue?.author?.rating} />
                 </View>
-              </TouchableOpacity>
-            ))}
-          </Animated.ScrollView>
-        </View>
-        <BottomNavigation navigation={navigation} />
-      </>
-    );
-  }
-}
+              </View>
+            </TouchableOpacity>
+          ))}
+        </Animated.ScrollView>
+      </View>
+      <BottomNavigation navigation={navigation} />
+    </>
+  );
+};
 
 const styleSheet = StyleSheet.create({
   markerWrap: {
     alignItems: 'center',
     justifyContent: 'center',
-    width: normalize(50),
-    height: normalize(50),
+    width: normalize(55),
+    height: normalize(55),
   },
   marker: {
-    width: normalize(35),
-    height: normalize(40),
+    width: normalize(30),
+    height: normalize(35),
   },
   scrollView: {
     position: 'absolute',
@@ -207,16 +233,18 @@ const styleSheet = StyleSheet.create({
     borderRadius: normalize(17),
     backgroundColor: '#FFF',
     marginHorizontal: normalize(10),
-    height: normalize(100),
-    width: normalize(250),
+    height: normalize(110),
+    width: normalize(300),
     overflow: 'hidden',
   },
   photo: {
-    height: normalize(80),
-    width: normalize(80),
+    height: normalize(90),
+    width: normalize(90),
     opacity: 1,
     position: 'relative',
     borderRadius: normalize(17),
     overflow: 'hidden',
   },
 });
+
+export default inject('Store')(observer(MapsScreen));
